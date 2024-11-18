ecf211ecd00d83a7dff11ac1d9a8cfb2d12aebb5
Tivra Raj <tivra@tomoclub.org>
2024-11-18 13:08:09 +0530

    Project Setup
    1. Download and Install SFML
    2. Assets

--- Main.cpp
+++ Main.cpp
@@ -1,21 +1,7 @@
-// TODO: There are no Namespaces being used
-
-#include <SFML/Graphics.hpp>
-#include "../../Header/Core/GameWindowManager.h"
-using namespace sf;
-using namespace Core;
+#include <iostream>
 
 int main()
 {
-    GameWindowManager * game_window_manager = new GameWindowManager();
-    game_window_manager->initialize();
-
-    while (game_window_manager->isGameRunning())
-    {
-        game_window_manager->pollEvents();
-        game_window_manager->update();
-        game_window_manager->render();
-    }
-
+    std::cout << "Hello World";
     return 0;
 }
