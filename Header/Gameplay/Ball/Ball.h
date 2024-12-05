edfdaa71804f98b60fbc49b73641edd247a10d9f
Aryan khera <aryan.khera@outscal.com>
2024-12-05 19:11:28 +0530

    Update Ball.h

--- Header/Gameplay/Ball/Ball.h
+++ Header/Gameplay/Ball/Ball.h
@@ -14,11 +14,11 @@ namespace Gameplay
 
         string texture_path = "Assets/Textures/Ball.png";
 
-        const float scale_x = 0.2f;
-        const float scale_y = 0.2f;
+        const float scale_x = 0.06f;
+        const float scale_y = 0.06f;
 
         const float position_x = 615.0f;
-        const float position_y = 335.0f;
+        const float position_y = 325.0f;
 
         void loadTexture();
         void initializeVariables();
